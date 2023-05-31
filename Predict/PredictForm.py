57880d2cf7a19a060a91db5dcdbb67bfe654a2d1
phongnt02 <phongnguyen.290102@gmail.com>
2023-06-01 01:01:10 +0700

    update conver money and render img recognition

--- Predict/PredictForm.py
+++ Predict/PredictForm.py
@@ -8,7 +8,7 @@ class PredictForm(forms.Form):
     Name = forms.ChoiceField(choices=dbCar,widget=forms.Select(),label='Tên dòng xe')
     ListYear= tuple((i, i) for i in range(2000, 2024, 1))
     Year = forms.ChoiceField(choices=ListYear,widget=forms.Select(),label='Năm sản xuất')
-    Price = forms.CharField(max_length=30, label='Giá tại thời điểm mua')
+    Price = forms.CharField(max_length=30, label='Giá tại thời điểm mua ($)')
     kmsDriven = forms.CharField(max_length=10, label='Số Kms đã đi được')
     fuelType = forms.ChoiceField(choices=(('Diesel','Diesel'),('Petrol','Petrol')),widget=forms.Select(),label='Loại nhiên liệu sử dụng')
     def __init__(self, *args, **kwargs):
